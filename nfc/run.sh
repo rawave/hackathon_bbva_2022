8e2f734eddb8266edbd2be0d23128c85bffc046d
Faustino Luna <faustino.luna.next@bbva.com>
2022-10-21 23:30:03 -0500

    Added docker compose

--- nfc/run.sh
+++ nfc/run.sh
@@ -1,2 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 uvicorn main:app --reload --host 0.0.0.0 --ssl-keyfile decrypted_key.pem --ssl-certfile cert.pem
