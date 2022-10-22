6adb186085bcb56ed940236b432a75505b79ac19
Faustino Luna <faustino.luna.next@bbva.com>
2022-10-22 13:05:21 -0500

    Delete unused script

--- nfc/run.sh
+++ nfc/run.sh
@@ -2,4 +2,3 @@
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 uvicorn main:app --reload --host 0.0.0.0 --port 8000
-#uvicorn main:app --reload --host 0.0.0.0 --port 8000 --ssl-keyfile decrypted_key.pem --ssl-certfile cert.pem
