146743295c6ec9dafa9dc4f7fb49ae0ce7f8c8b4
Faustino Luna <faustino.luna.next@bbva.com>
2022-10-21 22:13:46 -0500

    TODOs

--- nfc/main.py
+++ nfc/main.py
@@ -1,3 +1,4 @@
+from tokenize import Name
 from urllib import response
 from fastapi import FastAPI, Response
 #from fastapi.responses import ORJSONResponse
@@ -20,5 +21,10 @@ async def root():
 
 @app.post("/nfc/api/v1/senddata")
 def api_send(data: NfcData):
-    response = ClientInfo(name="Faus")
-    return response
+    return getClientData(data)
+
+def getClientData(data: NfcData):
+    # TODO: Validate client in db
+    # TODO: Get client info
+    client_info = ClientInfo(name="Faus")
+    return client_info
