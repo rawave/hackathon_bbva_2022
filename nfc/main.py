39a9905018e60d1dbeb89833f61439a52404068a
Faustino Luna <faustino.luna.next@bbva.com>
2022-10-21 21:34:38 -0500

    Added endpoints

--- nfc/main.py
+++ nfc/main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI, Response
 #from fastapi.responses import ORJSONResponse
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class NfcData(BaseModel):
+    client_id: str
+
+class ClientInfo(BaseModel):
+    name: str
+
 @app.get("/nfc")
-async def main():
+async def root():
     with open("web/auth.html") as htmlFile:
         data = htmlFile.read()
     return Response(content=data, media_type="text/html", status_code=200)
+
+@app.post("/nfc/api/v1/senddata")
+def api_send(data: NfcData):
+    return Response(status_code=200)
+
+@app.get("/nfc/api/v1/clientinfo")
+def api_get():
+    data = ClientInfo(name="Faus")
+    return data
--- nfc/run.sh
+++ nfc/run.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
-uvicorn main:app --reload --host 0.0.0.0 --ssl-keyfile key.pem --ssl-certfile cert.pem
+#!/bin/zsh
+uvicorn main:app --reload --host 0.0.0.0 --ssl-keyfile decrypted_key.pem --ssl-certfile cert.pem
